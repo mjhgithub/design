5eeeaf7c1b99bd5b189b0d5424fb9eaa96251b5f
majunhang <majunhang@netposa.com>
2019-12-05 19:02:10 +0800

    +adapter

---
+++ src/main/java/com/mark/icohome/design/adapter/AdvancedMediaPlayer.java
@@ -0,0 +1,11 @@
+package com.mark.icohome.design.adapter;
+
+/**
+ * @author mjh
+ */
+
+public interface AdvancedMediaPlayer {
+    public void playVlc(String fileName);
+
+    public void playMp4(String fileName);
+}
---
+++ src/main/java/com/mark/icohome/design/adapter/MediaPlayer.java
@@ -0,0 +1,9 @@
+package com.mark.icohome.design.adapter;
+
+/**
+ * @author mjh
+ */
+
+public interface MediaPlayer {
+    public void play(String audioType, String fileName);
+}
