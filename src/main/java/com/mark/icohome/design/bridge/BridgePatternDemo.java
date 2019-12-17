bd8df4ea1f01a2d376aa0dcb6928d068d182e4e7
majunhang <2479427380@qq.com>
2019-12-17 21:25:05 +0800

    +bridge

---
+++ src/main/java/com/mark/icohome/design/bridge/BridgePatternDemo.java
@@ -0,0 +1,20 @@
+package com.mark.icohome.design.bridge;
+
+public class BridgePatternDemo {
+    public static void main(String[] args) {
+        Shape redCircle = new Circle(100, 100, 10, new RedCircle());
+        Shape greenCircle = new Circle(100, 100, 10, new GreenCircle());
+
+        redCircle.draw();
+        greenCircle.draw();
+
+        Shape shape = new Shape(new GreenCircle()) {
+            @Override
+            public void draw() {
+                System.out.println("Drawing Shape");
+            }
+        };
+        shape.draw();
+
+    }
+}
