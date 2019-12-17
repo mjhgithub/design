02cf1af9925789166aad16c5d088d35788708416
majunhang <2479427380@qq.com>
2019-12-17 21:45:59 +0800

    +facade

---
+++ src/main/java/com/mark/icohome/design/facade/Shape.java
@@ -0,0 +1,5 @@
+package com.mark.icohome.design.facade;
+
+public interface Shape {
+    void draw();
+}
---
+++ src/main/java/com/mark/icohome/design/facade/Square.java
@@ -0,0 +1,13 @@
+package com.mark.icohome.design.facade;
+
+
+/**
+ * @author mjh
+ */
+
+public class Square implements Shape {
+    @Override
+    public void draw() {
+        System.out.println("Inside Square::draw() method.");
+    }
+}
